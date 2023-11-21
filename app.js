f963b7b0fe50aa1cf4541d6a903f9292a65dbe3e
Juan Granke <juangranke@outlook.com>
2023-11-21 17:13:59 -0300

    adicionado morgan

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const morgan = require('morgan')
+const moment = require('moment')
+
+morgan.token('customDate', () => moment().format('DD/MM/YYYY HH:mm:ss'))
+morgan.token('statusColor', (req, res) => {
+  const status = (typeof res.headersSent !== 'boolean' ? Boolean(res.header) : res.headersSent) ? res.statusCode : undefined
+  const color = status >= 500 ? 31  // red
+    : status >= 400 ? 33            // yellow
+      : status >= 300 ? 36          // cyan
+        : status >= 200 ? 32        // green
+          : 0                       // no color
+  return '\x1b[' + color + 'm' + status + '\x1b[0m'
+})
+
+app.use(morgan(':customDate \x1b[33m:method\x1b[0m :url :statusColor - :response-time ms - :res[content-length]'))
 
 app.use(cors())
 app.use(express.json())
