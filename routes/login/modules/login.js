b25f23590d4fb274d4663533198a258102506b17
Juan Granke <juangranke@outlook.com>
2023-11-26 20:39:28 -0300

    ajuste login app

--- routes/login/modules/login.js
+++ routes/login/modules/login.js
@@ -8,25 +8,29 @@ module.exports = (email, password) => {
   return new Promise(async (resolve, reject) => {
 
     try {
+        console.log(11, email, password)
         let user = await dbQuery(dbConfig, searchUser, [email.trim()])
-        if(user.length == 0) reject({ mensagem: 'E-mail não encontrado ou inválido.' })
         console.log(13, user)
-        if(bcrypt.compareSync(password, user[0].password))
-            resolve({
-                mensagem: 'Usuário logado com sucesso',
-                dados: {
-                    id: user[0].id,
-                    full_name: user[0].full_name,
-                    email: user[0].email,
-                    permission: user[0].permission
-                }
-            })
+        if(user.length == 0) reject({ mensagem: 'E-mail não encontrado ou inválido.' })
         else {
-            console.log('Aqui fdp')
-            reject({
-                mensagem: 'Senha informada inválida.',
-            })
-        }
+            console.log(13, user)
+            if(bcrypt.compareSync(password, user[0].password))
+                resolve({
+                    mensagem: 'Usuário logado com sucesso',
+                    dados: {
+                        id: user[0].id,
+                        full_name: user[0].full_name,
+                        email: user[0].email,
+                        permission: user[0].permission
+                    }
+                })
+            else {
+                console.log('Aqui fdp')
+                reject({
+                    mensagem: 'Senha informada inválida.',
+                })
+            }
+        }   
     } catch(err) {
         console.log(31, err)
         reject({
