58937c61beb91b278499f4c377df11e1415456ee
Juan Granke <juangranke@outlook.com>
2023-11-26 15:02:43 -0300

    console.log config banco

--- config/database/dbQuery.js
+++ config/database/dbQuery.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2/promise')
 
 module.exports = async (dbConfig, sql, params = []) => {
     try {
+        console.log(5, dbConfig)
         let connection = await mysql.createConnection(dbConfig)
         const [rows, fields] = await connection.query(sql, params)
         await connection.end()
